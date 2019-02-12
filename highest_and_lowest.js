efa561e6d6ef76a8494524f1ec7eb6de1635319d
Khaya <yahyal0v3@yahoo.com>
2019-02-11 20:37:27 -0500

    Add problem description

--- highest_and_lowest.js
+++ highest_and_lowest.js
@@ -1,3 +1,7 @@
+/*
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+*/
+
 function highAndLow(numbers){
   let sortedNumArray = numbers.split(" ").sort((a, b) => a - b)
   /* Not working for test '42 42' for some reason. 
