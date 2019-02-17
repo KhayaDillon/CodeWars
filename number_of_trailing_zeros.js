557e35b4b3ea05b88b1461b841eb3667bbd7eb38
Khaya <yahyal0v3@yahoo.com>
2019-02-16 19:31:48 -0500

    Add problem description

--- number_of_trailing_zeros.js
+++ number_of_trailing_zeros.js
@@ -1,3 +1,14 @@
+/*
+Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+Examples
+  zeros(6) => 1
+  # 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+
+  zeros(12) => 2
+  # 12! = 479001600 --> 2 trailing zeros
+*/
+
 function zeros (num) {
   const numToMultiply = [] 
   for (n = 1; n <= num; n++) {
