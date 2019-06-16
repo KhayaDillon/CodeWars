525a59a97d5361158d035051f25b61aef01d9d01
Khaya R. Dillon <yahyal0v3@yahoo.com>
2019-06-16 13:10:32 -0400

    Create consonant js file and function

---
+++ JavaScript/consonant_count.js
@@ -0,0 +1,18 @@
+
+function consonantCount(str) {
+    str=str.toLowerCase();
+    var vow='aeiou';
+    var cons='abcdefghijklmnopqrstuvwxyz';
+    var i=0;
+    var n=0;
+    
+    while(i<str.length)
+    {
+      if(cons.indexOf(str[i])!==-1 && vow.indexOf(str[i])===-1)
+      {
+        n++;
+      }
+      i++;
+    }
+    return n;
+}
