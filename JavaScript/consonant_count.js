480c4864a7c60cdaea9d65a2566021c7ef86b106
Khaya R. Dillon <yahyal0v3@yahoo.com>
2019-06-16 13:12:48 -0400

    Add vowelCount function

--- JavaScript/consonant_count.js
+++ JavaScript/consonant_count.js
@@ -16,3 +16,21 @@ function consonantCount(str) {
     }
     return n;
 }
+
+function vowelCount(str) {
+    str=str.toLowerCase();
+    var vow='aeiou';
+    var cons='abcdefghijklmnopqrstuvwxyz';
+    var i=0;
+    var n=0;
+    
+    while(i<str.length)
+    {
+      if(cons.indexOf(str[i])===-1 && vow.indexOf(str[i])!==-1)
+      {
+        n++;
+      }
+      i++;
+    }
+    return n;
+}
