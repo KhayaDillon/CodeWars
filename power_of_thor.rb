bec57471bd40202e449e1b07497e75f5835b2551
Khaya <yahyal0v3@yahoo.com>
2019-09-04 00:24:40 -0400

    Update power_of_thor.rb

--- power_of_thor.rb
+++ power_of_thor.rb
@@ -23,3 +23,41 @@ if light_x > initial_tx
             puts "N"
         end
     end
+
+#Figuring out Case 3
+
+if initial_ty == 17 || initial_ty == 0
+        if light_x > initial_tx
+            puts "E"
+        else
+            puts "W"
+        end
+    elsif initial_tx == 39 || initial_tx == 0
+        if light_y > initial_ty
+            puts "S"
+        else
+            puts "N"
+        end
+    elsif light_x > initial_tx
+        if light_y > initial_ty
+            puts "SE"
+        elsif light_y < initial_ty
+            puts "NE"
+        else
+            puts "E"
+        end
+    elsif light_x < initial_tx
+        if light_y > initial_ty
+            puts "SW"
+        elsif light_y < initial_ty
+            puts "NW"
+        else
+            puts "W"
+        end
+    else
+        if light_y > initial_ty
+            puts "S"
+        elsif light_y < initial_ty
+            puts "N"
+        end
+    end
